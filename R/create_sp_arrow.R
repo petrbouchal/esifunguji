515bdd39a6262d8f4468b46d320c9f0f2f2247e4
Petr Bouchal <pbouchal@gmail.com>
2021-04-04 14:48:15 +0200

    keep+partition by druh and period_vykaz in arrow datasets

--- R/create_sp_arrow.R
+++ R/create_sp_arrow.R
@@ -1,4 +1,4 @@
-make_yearly_consolidated_budget <- function(table_file, polozka_codelist, months) {
+load_and_prep_budget <- function(table_file, polozka_codelist, months) {
 
   polozka <- polozka_codelist
 
@@ -16,14 +16,15 @@ load_budget_yearsum_local <- function(table_file, months, codelists) {
   polozka_branchname <- names(codelists)[str_detect(names(codelists), "polozka$")]
   polozka <- codelists[[polozka_branchname]]
 
-  b_base <- make_yearly_consolidated_budget(table_file, polozka, months)
+  b_base <- load_and_prep_budget(table_file, polozka, months)
 
   rslt <- b_base %>%
     filter(!kon_pol | !kon_rep | !kon_okr | !kon_kraj) %>%
     filter(per_m %in% months) %>%
     group_by(nuts, kraj, .add = T) %>%
     summarise(across(c(starts_with("budget")), sum, na.rm = T),
-              .groups = "drop")
+              .groups = "drop") %>%
+    mutate(period_vykaz = lubridate::make_date(per_yr, 12, 31))
   return(rslt)
 }
 
@@ -34,14 +35,15 @@ load_budget_yearsum_central_old <- function(table_file, months, codelists) {
   polozka_branchname <- names(codelists)[str_detect(names(codelists), "polozka$")]
   polozka <- codelists[[polozka_branchname]]
 
-  b_base <- make_yearly_consolidated_budget(table_file, polozka, months)
+  b_base <- load_and_prep_budget(table_file, polozka, months)
 
   rslt <- b_base %>%
     filter(!kon_pol | kon_rep | kon_okr | kon_kraj) %>%
     filter(per_m %in% months) %>%
     group_by(kapitola, .add = TRUE) %>%
     summarise(across(c(starts_with("budget")), sum, na.rm = T),
-              .groups = "drop")
+              .groups = "drop") %>%
+    mutate(period_vykaz = lubridate::make_date(per_yr, 12, 31))
 
   return(rslt)
 }
@@ -54,13 +56,14 @@ load_budget_yearsum_central_new <- function(table_file, months,
   polozka_branchname <- names(codelists)[str_detect(names(codelists), "polozka$")]
   polozka <- codelists[[polozka_branchname]]
 
-  b_base <- make_yearly_consolidated_budget(table_file, polozka, months)
+  b_base <- load_and_prep_budget(table_file, polozka, months)
 
   rslt <- b_base %>%
     filter(!kon_pol | kon_rep | kon_okr | kon_kraj) %>%
     group_by(kapitola, zdroj, pvs, .add = TRUE) %>%
     summarise(across(c(starts_with("budget")), sum, na.rm = T),
-              .groups = "drop")
+              .groups = "drop") %>%
+    mutate(period_vykaz = lubridate::make_date(per_yr, 12, 31))
   return(rslt)
 }
 
@@ -80,7 +83,7 @@ budget_arrow_months <- function(table_file, destination, months, fn, codelists) 
   purrrow::marrow_dir(table_file, fn,
                       months = months, codelists = codelists,
                       .path = here::here(destination),
-                      .partitioning = c("per_yr"))
+                      .partitioning = c("per_yr", "period_vykaz", "druh"))
 
 }
 
@@ -93,5 +96,6 @@ load_budget_local_grants <- function(table_file, months, codelists) {
   sp_load_table(table_file) %>%
     sp_add_codelist(ucelznak) %>%
     filter(per_m %in% months) %>%
-    rename(budget_grants = ZU_ROZKZM)
+    rename(budget_grants = ZU_ROZKZM) %>%
+    mutate(druh = "Příjmy")
 }
