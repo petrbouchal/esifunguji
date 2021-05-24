d98459c8f74b3040b9df011bad4f4aada7c82541
Petr Bouchal <pbouchal@gmail.com>
2021-05-24 23:18:50 +0200

    update interactive helper

--- _interactive.R
+++ _interactive.R
@@ -1,8 +1,10 @@
 # script for quickly loading
 
 source("_targets_packages.R")
-source("R/functions.R")
 source("R/utils.R")
+source("R/functions.R")
+
+options(scipen = 9)
 
 ts <- as.list(targets::tar_manifest(fields = name)[["name"]])
 names(ts) <- ts
